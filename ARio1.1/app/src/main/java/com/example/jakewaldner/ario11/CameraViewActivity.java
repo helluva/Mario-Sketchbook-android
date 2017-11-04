394a5f0b717fbb1f95f34046beb251c3e25a8ff4
JakeWaldner <jakew.waldner@gmail.com>
2017-11-04 16:40:26 -0400

    Created general views and xml elements

---
+++ ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/CameraViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.jakewaldner.ario11;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by jakewaldner on 11/4/17.
+ */
+
+public class CameraViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cameraview);
+
+
+    }
+}
--- ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/MainActivity.java
+++ ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jakewaldner.ario11;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,35 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //handles transition to cameraview
+        Button goToCodesListButton = (Button) this.findViewById(R.id.gotocamera_button);
+        goToCodesListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this, CameraViewActivity.class);
+                MainActivity.this.startActivity(i);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
