adffed1f277bba0ad1ddb0d677394964278c8adf
JakeWaldner <jakew.waldner@gmail.com>
2017-11-04 17:11:12 -0400

    added camera implemantation stuff to cameraViewActivity

--- ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/CameraViewActivity.java
+++ ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/CameraViewActivity.java
@@ -1,24 +1,109 @@
 package com.example.jakewaldner.ario11;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
 /**
  * Created by jakewaldner on 11/4/17.
  */
 
-public class CameraViewActivity extends Activity {
+public class CameraViewActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener {
+    Context context = this;
+    private static final String TAG = "AndroidCameraApi";
+
+    //this is to ensure that openCV is async loaded
+    Mat testMat;
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    //open CV's camera requires an async call
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("OpenCV", "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    testMat = new Mat();
+                    //an online example instantiated all Mats here
+
+                } break;
+                default: {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 100);
+        }
+
         setContentView(R.layout.activity_cameraview);
 
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.image_manipulations_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+        //startBackgroundThread();
+        /*if (textureView.isAvailable()) {
+            openCamera();
+        } else {
+            textureView.setSurfaceTextureListener(textureListener);
+        }*/
+
+        //checked if OpenCV library have been loaded and initialized from within current application package or not
+        //THIS IS FOR THE MAT ERROR
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, CameraViewActivity.this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "onPause");
+        //closeCamera();
+        //stopBackgroundThread();
+        super.onPause();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 }
