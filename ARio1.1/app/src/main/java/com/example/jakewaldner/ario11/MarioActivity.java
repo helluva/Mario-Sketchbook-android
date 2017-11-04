527df8d7f4cb7dfe32bbde36d0ed2faafdba6f34
Cal Stephens <cal@calstephens.tech>
2017-11-04 17:45:48 -0400

    Add MarioActivity

---
+++ ARio1.1/app/src/main/java/com/example/jakewaldner/ario11/MarioActivity.java
@@ -0,0 +1,20 @@
+package com.example.jakewaldner.ario11;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mario);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
